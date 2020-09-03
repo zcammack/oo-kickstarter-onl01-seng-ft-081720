9c587f5d10b86af0f18ebb0c77308a0e795feb3e
Zackary Cammack <zack.cammack@gmail.com>
2020-09-03 00:08:43 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -3,19 +3,13 @@ class Project
   attr_reader :title
   attr_accessor :backers
 
-  @@all = []
-
   def initialize(title)
     @title = title
     @backers = []
-    @@all << self
-  end
-
-  def self.all
-    @@all
   end
 
   def add_backer(backer)
     @backers << backer
+    backed_projects.backer << self
   end
 end
